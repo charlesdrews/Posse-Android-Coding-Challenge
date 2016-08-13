5214f7da8fcb178c574866ef8a009c0cba07df4f
charlesdrews <charlesdrews@gmail.com>
2016-08-13 09:14:40 -0400

    Add empty Android Studio project

---
+++ CharlesDrewsDemoApp/app/src/main/java/com/charlesdrews/charlesdrewsdemoapp/PersonListFragment.java
@@ -0,0 +1,22 @@
+package com.charlesdrews.charlesdrewsdemoapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PersonListFragment extends Fragment {
+
+    public PersonListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_person_list, container, false);
+    }
+}
